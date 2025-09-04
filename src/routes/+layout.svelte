d6e358f36fe969edeb518f3631c59930a73956bf
michaelshumshum <36499322+michaelshumshum@users.noreply.github.com>
2025-09-03 23:53:28 -0700

    feat: use carbon styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
 import favicon from "$lib/assets/favicon.svg";
+import "carbon-components-svelte/css/all.css";
+import { onMount } from "svelte";
 
 let { children } = $props();
+
+onMount(() => {
+  document.documentElement.setAttribute("theme", "g100");
+});
 </script>
 
 <svelte:head>
