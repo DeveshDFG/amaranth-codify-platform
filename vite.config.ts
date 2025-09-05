0b457f7efa715c617a02a3d0bdd2e134c91b7279
michaelshumshum <36499322+michaelshumshum@users.noreply.github.com>
2025-09-05 13:03:39 -0700

    fix: add devtools json

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import devtoolsJson from "vite-plugin-devtools-json";
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [devtoolsJson(), sveltekit()],
   test: {
     expect: { requireAssertions: true },
     projects: [
