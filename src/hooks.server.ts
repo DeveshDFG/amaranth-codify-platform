3bcd17ffa1e9870a68554f53178f729f7fe1368c
michaelshumshum <36499322+michaelshumshum@users.noreply.github.com>
2025-09-06 16:39:18 -0700

    feat: set up better auth core

---
+++ src/hooks.server.ts
@@ -0,0 +1,7 @@
+import { svelteKitHandler } from "better-auth/svelte-kit";
+import { building } from "$app/environment";
+import { auth } from "$lib/auth";
+
+export async function handle({ event, resolve }) {
+  return svelteKitHandler({ event, resolve, auth, building });
+}
