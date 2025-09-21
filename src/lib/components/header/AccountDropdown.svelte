f41176f1a26b3914f622dc31c8ab6755156b118a
michaelshumshum <36499322+michaelshumshum@users.noreply.github.com>
2025-09-21 01:51:23 -0700

    feat: check if signed in

--- src/lib/components/header/AccountDropdown.svelte
+++ src/lib/components/header/AccountDropdown.svelte
@@ -5,9 +5,24 @@ import {
   Theme,
 } from "carbon-components-svelte";
 import { User } from "carbon-icons-svelte";
+import { onMount } from "svelte";
+
+let signedIn: boolean = $state(false);
+
+onMount(async () => {
+  let response = await fetch("/api/me");
+  if (response.ok) {
+    signedIn = true;
+  }
+});
 </script>
 
 <OverflowMenu icon={User} flipped>
-    <OverflowMenuItem text="Sign In" href="/sign-in"/>
-    <OverflowMenuItem text="Sign Up" href="/sign-up"/>
+    {#if signedIn}
+        <OverflowMenuItem text="Account" href="/account"/>
+        <OverflowMenuItem text="Sign Out" href="/sign-out"/>
+    {:else}
+        <OverflowMenuItem text="Sign In" href="/sign-in"/>
+        <OverflowMenuItem text="Sign Up" href="/sign-up"/>
+    {/if}
 </OverflowMenu>
