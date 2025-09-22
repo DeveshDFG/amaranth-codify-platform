90a748afa6aa09c08f5852717cc74600f7184d7a
michaelshumshum <36499322+michaelshumshum@users.noreply.github.com>
2025-09-22 12:28:55 -0700

    feat: sign out page

---
+++ src/routes/sign-out/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { onMount } from "svelte";
+import { authClient } from "$lib";
+
+let signedOut = $state(false);
+
+onMount(async () => {
+  await authClient.signOut();
+  signedOut = true;
+});
+</script>
+
+<div>
+    {#if signedOut}
+        <h1>Signed Out</h1>
+        <p>You have been successfully signed out. <a href="/sign-in">Sign in again</a></p>
+    {:else}
+        <h1>Signing Out...</h1>
+        <p>Please wait while we sign you out.</p>
+    {/if}
+</div>
+
+<style>
+    div {
+        padding: 4em;
+        display: flex;
+        flex-direction: column;
+        gap: 1em;
+    }
+</style>
