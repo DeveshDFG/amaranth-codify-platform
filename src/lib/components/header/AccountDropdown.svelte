f9edc655eff041c539861e3a0c684abd7d6bba3f
michaelshumshum <36499322+michaelshumshum@users.noreply.github.com>
2025-09-24 17:34:52 -0700

    refactor: rename variable and add comment

--- src/lib/components/header/AccountDropdown.svelte
+++ src/lib/components/header/AccountDropdown.svelte
@@ -3,15 +3,15 @@ import { OverflowMenu, OverflowMenuItem } from "carbon-components-svelte";
 import { User } from "carbon-icons-svelte";
 import { authClient } from "$lib/auth-client";
 
-let loggedIn = $state(false);
+let signedIn = $state(false);
 let open = $state(false);
 </script>
 
 <OverflowMenu bind:open={open} icon={User} flipped on:click={async () => {
-    if (open) return;
-    loggedIn = !!(await authClient.getSession()).data
+    if (open) return; // only check when opening the menu
+    signedIn = !!(await authClient.getSession()).data
 }}>
-    {#if loggedIn}
+    {#if signedIn}
         <OverflowMenuItem text="Account" href="/account"/>
         <OverflowMenuItem text="Sign Out" href="/sign-out"/>
     {:else}
