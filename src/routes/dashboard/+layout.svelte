89bf8d71ddde75618ba115c07b5c37f7d9454a32
michaelshumshum <36499322+michaelshumshum@users.noreply.github.com>
2025-10-24 01:39:44 -0700

    feat: dashboard layout and home page

---
+++ src/routes/dashboard/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { SideNavBar } from "$lib/components/dashboard/SideNavBar";
+
+let { children } = $props();
+</script>
+
+<div id="dashboard-layout">
+    <SideNavBar />
+    <div>
+        {@render children?.()}
+    </div>
+</div>
+
+<style>
+    div#dashboard-layout {
+        display: flex;
+        width: 100%;
+        flex-direction: row;
+        gap: 2em;
+    }
+</style>
