6f4e4ade5843a0bb2e9814c8e98621ecc4f0f1aa
michaelshumshum <36499322+michaelshumshum@users.noreply.github.com>
2025-10-25 05:22:24 -0700

    fix: update biomejson and rerun format and lint
    rename some components that conflict with canonical html tags. ref:
    https://github.com/biomejs/biome/issues/7864

--- src/lib/components/header/AccountDropdown.svelte
+++ src/lib/components/header/AccountDropdown.svelte
@@ -7,15 +7,20 @@ let signedIn = $state(false);
 let open = $state(false);
 </script>
 
-<OverflowMenu bind:open={open} icon={User} flipped on:click={async () => {
+<OverflowMenu
+  bind:open={open}
+  icon={User}
+  flipped
+  on:click={async () => {
     if (open) return; // only check when opening the menu
     signedIn = !!(await authClient.getSession()).data
-}}>
-    {#if signedIn}
-        <OverflowMenuItem text="Account" href="/account"/>
-        <OverflowMenuItem text="Sign Out" href="/sign-out"/>
-    {:else}
-        <OverflowMenuItem text="Sign In" href="/sign-in"/>
-        <OverflowMenuItem text="Sign Up" href="/sign-up"/>
-    {/if}
+}}
+>
+  {#if signedIn}
+  <OverflowMenuItem text="Account" href="/account"/>
+  <OverflowMenuItem text="Sign Out" href="/sign-out"/>
+  {:else}
+  <OverflowMenuItem text="Sign In" href="/sign-in"/>
+  <OverflowMenuItem text="Sign Up" href="/sign-up"/>
+  {/if}
 </OverflowMenu>
--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -4,29 +4,29 @@ import ThemeSwitcher from "./ThemeSwitcher.svelte";
 </script>
 
 <header>
-    <h6>Amaranth Codify</h6>
-    <div>
-        <AccountDropdown/>
-        <ThemeSwitcher/>
-    </div>
+  <h6>Amaranth Codify</h6>
+  <div>
+    <AccountDropdown/>
+    <ThemeSwitcher/>
+  </div>
 </header>
 
 <style>
-    header {
-        position: fixed;
-        width: 100%;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: 0 4em;
-        height: 3rem;
-        border-bottom: 1px solid var(--cds-border-strong);
-    }
+header {
+  position: fixed;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 4em;
+  height: 3rem;
+  border-bottom: 1px solid var(--cds-border-strong);
+}
 
-    div {
-        display: flex;
-        flex-direction: row;
-        align-items: center;
-        justify-content: space-between;
-    }
+div {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+}
 </style>
