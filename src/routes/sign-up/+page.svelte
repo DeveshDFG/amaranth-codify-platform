e97211926fa86d31382f855c19f8adb3f548ce08
thaoxpham <gabong2001.hhk@gmail.com>
2025-10-27 11:21:32 -0700

    fix: little typo, change to original

--- src/routes/sign-up/+page.svelte
+++ src/routes/sign-up/+page.svelte
@@ -46,8 +46,8 @@ const { startLoading, stopLoading } = useLoading();
 
 <div>
     {#if !form || !form.success}
-        <h1>Sign Up</h1>LAgger
-        
+        <h1>Sign Up</h1>
+
         <p>Already have an account? <a href="/sign-in">Sign in</a></p>
         {#if !form?.success && form?.message}
             <p style="color: red">{form.message}</p>
