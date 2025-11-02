873fb2794577c5beead81f31a48a1551bbe8b318
deveshmo <dmohanta@asu.edu>
2025-11-02 14:20:59 -0700

    feat: added svg icons, added linkedin auth

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -9,3 +9,8 @@ export const authClient = createAuthClient({
 export const signInGoogle = async () => {
   await authClient.signIn.social({ provider: "google" });
 };
+
+export const signInLinkedIn = async () => {
+  await authClient.signIn.social({ provider: "linkedin" });
+};
+
